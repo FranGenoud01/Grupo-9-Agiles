c7192acf039ba75a399db64ebdcef8a3450ee5e1
Franco <francogenoud1@gmail.com>
2026-06-23 18:43:17 -0300

    RED: UT - Agrega tests para estado de derrota y revelacion de palabra

--- tests/ahorcado.test.ts
+++ tests/ahorcado.test.ts
@@ -6,20 +6,24 @@ describe("Ahorcado - Iniciar Partida", () => {
     const juego = new Ahorcado("GATO");
     expect(juego.palabraEnmascarada()).toBe("_ _ _ _");
   });
+
   it("inicia la partida con 6 vidas", () => {
     const juego = new Ahorcado("GATO");
     expect(juego.vidasRestantes()).toBe(6);
   });
+
   it("revela la letra acertada en la palabra enmascarada", () => {
     const juego = new Ahorcado("GATO");
     juego.adivinar("A");
     expect(juego.palabraEnmascarada()).toBe("_ A _ _");
   });
+
   it("descuenta una vida cuando la letra es incorrecta", () => {
     const juego = new Ahorcado("GATO");
     juego.adivinar("E");
     expect(juego.vidasRestantes()).toBe(5);
   });
+
   it("indica que el juego esta ganado cuando se adivinan todas las letras", () => {
     const juego = new Ahorcado("GATO");
     juego.adivinar("G");
@@ -27,5 +31,11 @@ describe("Ahorcado - Iniciar Partida", () => {
     juego.adivinar("T");
     juego.adivinar("O");
     expect(juego.estado()).toBe("GANASTE");
+  });
+
+  it("indica que el juego esta perdido cuando se acaban las vidas", () => {
+    const juego = new Ahorcado("GATO");
+    ["Z", "X", "C", "V", "B", "N"].forEach(letra => juego.adivinar(letra));
+    expect(juego.estado()).toBe("PERDISTE");
   });
 });
