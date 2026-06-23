3341752891fc388ea77f7e4e3fe26ccaaab291d5
Franco <francogenoud1@gmail.com>
2026-06-22 21:38:49 -0300

    GREEN: UT - Implementa logica de vidas iniciales

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,7 +1,13 @@
 export class Ahorcado {
+  private vidas: number = 6;
+
   constructor(private palabraSecreta: string) {}
 
   palabraEnmascarada(): string {
     return this.palabraSecreta.replace(/./g, "_ ").trim();
+  }
+
+  vidasRestantes(): number {
+    return this.vidas;
   }
 }
